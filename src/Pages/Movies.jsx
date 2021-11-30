8ed4578d5cd85aa4cd7efc23cb5c1116a4942e0c
Kolawole <92513093+adeoye99@users.noreply.github.com>
2021-11-30 14:59:38 +0100

    changes in the arrangement of the movie page

--- src/Pages/Movies.jsx
+++ src/Pages/Movies.jsx
@@ -10,68 +10,58 @@ import {Link} from 'react-router-dom';
 
 function Mylist (){
 return (<div>
-		<div className = "movies">
 		<Link to='/video1'>
 	        <Image 
                 id = "10"
                 img = "/Movieimages/avengers.jpg" 
                 alt = "alien"
-                 />
-		</div>
-
-		<div className = "movies">
+                 /></Link>
+	
 		<Link to='/video2'> <Image 
                 id = "11"
                 img = "/Movieimages/inception.jpg" 
                 alt = "inception" topic="inception:"
                about="Skilled in the act of subconscious security, a man and his gand are hired to get information from a billionaire son in this tale." 
-                  />
-	        </div>
-
-		<div className = "movies">
-		<Link to='/video3'>   <Image 
+                  /></Link>
+	        
+		<Link to='/video3'>   
+                <Image 
                 id = "12"
                 img = "/Movieimages/joker.jpg" 
                 alt = "joker" topic="Alien:"
                about='This is a fiction movie which tells how a child survived in space.'
                   />
-	      </div>
-
-		<div className = "movies">
+         	</Link>
+	     
 		<Link to='/video4'>   <Image 
                 id = "13"
                 img = "/Movieimages/alien.jpg" 
                 alt = "alien" topic="Alien:"
                about='This is a fiction movie which tells how a child survived in space.'
-                   /> 
-              </div>
+                   /></Link> 
+              
 		<div className = "movies">
 		<Link to='/video5'>   <Image
                  id = "14"
                  img ="/Movieimages/moneyheist3.jpg"
-                />      
-	       </div>
-		<div className = "movies">
+                />   </Link> 
+	
 		<Link to='/video6'> <Image
-                 id = "14"
+                 id = "15"
                  img ="/Movieimages/moneyheist3.jpg"
-                  />
-	         </div>
-
-		<div className = "movies">
+                  /></Link>
+	     
 		<Link to='/video7'>   <Image
-              id = "14"
-              img ="/Movieimages/moneyheist3.jpg"
-              /> 
-	       </div>
-
-		<div className = "movies">
+                  id = "16"
+                  img ="/Movieimages/moneyheist3.jpg"
+                  /> </Link>
+	
 		<Link to='/video8'>
-			   <Image
-                      id = "14"
-                   img ="/Movieimages/moneyheist3.jpg"
-                  />
-	</div>
+	            <Image
+                       id = "17"
+                       img ="/Movieimages/moneyheist3.jpg"
+                  /></Link>
+	         
 	</div>
 );
 };
